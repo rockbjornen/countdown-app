03d09b534fe5f470bc562af11a1982dc6e018718
björn <bjorn.lundkvist@netlight.com>
2018-10-26 20:37:12 +0200

    Updates

---
+++ precache-manifest.0795782f00012b393eeab97ca67af26a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6877d92f4722f96e1a1ecb8440a5d4c1",
+    "url": "/bb-countdown/static/media/shantaram.6877d92f.jpg"
+  },
+  {
+    "revision": "a134cb340675e7551d576cc481b5f729",
+    "url": "/bb-countdown/static/media/india.a134cb34.png"
+  },
+  {
+    "revision": "f3695628bf2a67714dc1",
+    "url": "/bb-countdown/static/js/runtime~main.f3695628.js"
+  },
+  {
+    "revision": "b9150c0e0e7cd202d85c",
+    "url": "/bb-countdown/static/js/main.b9150c0e.chunk.js"
+  },
+  {
+    "revision": "f0a40516b2b32c206f7d",
+    "url": "/bb-countdown/static/js/1.f0a40516.chunk.js"
+  },
+  {
+    "revision": "b9150c0e0e7cd202d85c",
+    "url": "/bb-countdown/static/css/main.3836c555.chunk.css"
+  },
+  {
+    "revision": "1c2c1205e3968202c7962f6f2a3cb68c",
+    "url": "/bb-countdown/index.html"
+  }
+];
